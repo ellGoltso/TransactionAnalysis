15e7eabbe948fbec9e08937ffb071fdd057fc36d
ellGoltso <goltsovi3@gmail.com>
2025-07-05 15:03:48 +0300

    write README

--- src/views.py
+++ src/views.py
@@ -24,7 +24,3 @@ def get_json_data(date: datetime.datetime):
 
     json_data = json.dumps(data, indent=4, ensure_ascii=False)
     return json_data
-
-
-# date_t = datetime.datetime(2021, 12, 16, 16, 27, 0)
-# print(get_json_data(date_t))
