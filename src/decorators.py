b1034203f451689bdf4eee9a87625b34e36d5540
ellGoltso <goltsovi3@gmail.com>
2025-07-05 13:49:50 +0300

    create test decorators

--- src/decorators.py
+++ src/decorators.py
@@ -11,10 +11,13 @@ def write_to_file(filename: str = None):
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
+
                 if filename:
-                    result.to_json(filename, force_ascii = False, indent=4, orient='records', date_format='iso')
+                    with open(filename, "w", encoding="utf-8") as f:
+                        result.to_json(f, force_ascii = False, indent=4, orient='records', date_format='iso')
                 else:
-                    result.to_json('data/function_results.json', force_ascii = False, indent=4, orient='records', date_format='iso')
+                    with open('data/function_results.json', "w", encoding="utf-8") as f:
+                        result.to_json(f, force_ascii = False, indent=4, orient='records', date_format='iso')
             except Exception as e:
                 exc_str = f"{func.__name__} error: {e}."
                 if filename:
---
+++ tests/test_decorators.py
@@ -0,0 +1,31 @@
+from src.decorators import write_to_file
+
+import datetime
+import pandas as pd
+import json
+
+
+def test_write_to_file():
+    @write_to_file("data/test_data.json")
+    def test_func():
+        return pd.DataFrame({"Column_1": [1, 2, 3, 4, 5], "Column_2": [5, 4, 3, 2, 1]})
+
+    test_func()
+
+    with open("data/test_data.json", "r", encoding="utf-8") as f:
+        content = json.load(f)
+
+    pd.testing.assert_frame_equal(pd.DataFrame(content), pd.DataFrame({'Column_1': [1, 2, 3, 4, 5], 'Column_2': [5, 4, 3, 2, 1]}))
+
+    @write_to_file("data/test_exc.json")
+    def test_exc():
+        return datetime.datetime(2024, 12, 2, 13, 2, 23)
+
+    test_exc()
+
+    with open("data/test_exc.json", "r", encoding="utf-8") as f:
+        content = json.load(f)
+
+    assert content == "test_exc error: 'datetime.datetime' object has no attribute 'to_json'."
+
+
